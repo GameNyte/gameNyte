b1eef2c0bde8e0a105cf171a65669d70104082fb
d-d-wolfe <dave.wolfe623@gmail.com>
2020-08-11 13:17:38 -0700

    added more form functionality, including custom hook

--- src/components/login-banner.js
+++ src/components/login-banner.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import TextField from '@material-ui/core/TextField';
 import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button';
+
+import useForm from '../hooks/use-form.js';
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -43,6 +46,14 @@ export default function SimpleModal() {
   const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
+  const [formData, setFormData] = useState({});
+  const [values, handleChange, handleLogin, handleCreateAccount ] = useForm(handleFormData);
+
+  function handleFormData(data) {
+    setFormData(data);
+    console.log('form has been submitted with ', data);
+  }
+
   const handleOpen = () => {
     setOpen(true);
   };
@@ -50,6 +61,16 @@ export default function SimpleModal() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  // const handleLogin = (event) => { // TODO: still needs to be completed.
+  //   event.preventDefault();
+  //   console.log('handleLogin has been clicked');
+  // };
+
+  // const handleCreateAccount = (event) => {
+  //   event.preventDefault();
+  //   console.log('handleCreateAccount has been clicked');
+  // };
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
@@ -60,17 +81,20 @@ export default function SimpleModal() {
 
       <a target="_blank" href="https://discord.com/api/oauth2/authorize?client_id=742459492511252501&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth&response_type=code&scope=identify">Login with Discord</a>
 
-      <form className={classes.root} noValidate autoComplete="off">
+      <form onSubmit={handleLogin} className={classes.root} noValidate autoComplete="off">
+
       <FormLabel component="legend">Login to Game Nyte</FormLabel>
-        <TextField id="filled-basic" label="User Name" variant="filled" />
-        <TextField id="filled-basic" label="Password" variant="filled" />
+        <TextField id="filled-basic" label="User Name" variant="filled" onChange={handleChange}/>
+        <TextField id="filled-basic" label="Password" variant="filled" onChange={handleChange}/>
+        <Button type="submit"  color="primary">Login</Button>
         
       </form>
       
-      <form className={classes.root} noValidate autoComplete="off">
+      <form onSubmit={handleCreateAccount} className={classes.root} noValidate autoComplete="off">
       <FormLabel component="legend">Create Account</FormLabel>
-        <TextField id="filled-basic" label="User Name" variant="filled" />
-        <TextField id="filled-basic" label="Password" variant="filled" />
+        <TextField id="filled-basic" label="User Name" variant="filled" onChange={handleChange}/>
+        <TextField id="filled-basic" label="Password" variant="filled" onChange={handleChange} />
+        <Button type="submit"  color="primary">Create Account</Button>
         
       </form>
 
