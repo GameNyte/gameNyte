17958917db72ed55de16bee9556f95cc62f385f2
d-d-wolfe <dave.wolfe623@gmail.com>
2020-08-11 16:15:05 -0700

    got state updated

--- src/components/login-banner.js
+++ src/components/login-banner.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import TextField from '@material-ui/core/TextField';
-import FormLabel from '@material-ui/core/FormLabel';
-import Button from '@material-ui/core/Button';
+import { makeStyles, Modal, TextField, FormLabel, Button, Input } from '@material-ui/core';
+import { connect } from 'react-redux';
+import { createAccount, login } from '../store/login.js';
 
-import useForm from '../hooks/use-form.js';
+
+
+// import useForm from '../hooks/use-form.js';
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -40,21 +40,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SimpleModal() {
+const SimpleModal = (props) => {
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
-  const [formData, setFormData] = useState({});
-  const [values, handleChange, handleLogin, handleCreateAccount ] = useForm(handleFormData);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  function handleFormData(data) {
-    setFormData(data);
-    console.log('form has been submitted with ', data);
-  }
-
-  const handleOpen = () => {
+    const handleOpen = () => {
     setOpen(true);
   };
 
@@ -62,16 +57,7 @@ export default function SimpleModal() {
     setOpen(false);
   };
 
-  // const handleLogin = (event) => { // TODO: still needs to be completed.
-  //   event.preventDefault();
-  //   console.log('handleLogin has been clicked');
-  // };
-
-  // const handleCreateAccount = (event) => {
-  //   event.preventDefault();
-  //   console.log('handleCreateAccount has been clicked');
-  // };
-
+ 
   const body = (
     <div style={modalStyle} className={classes.paper}>
       <h2 id="simple-modal-title">Login or create account</h2>
@@ -81,24 +67,42 @@ export default function SimpleModal() {
 
       <a target="_blank" href="https://discord.com/api/oauth2/authorize?client_id=742459492511252501&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth&response_type=code&scope=identify">Login with Discord</a>
 
-      <form onSubmit={handleLogin} className={classes.root} noValidate autoComplete="off">
+      <form   
+         className={classes.root} noValidate autoComplete="off">
 
       <FormLabel component="legend">Login to Game Nyte</FormLabel>
-        <TextField id="filled-basic" label="User Name" variant="filled" onChange={handleChange}/>
-        <TextField id="filled-basic" label="Password" variant="filled" onChange={handleChange}/>
-        <Button type="submit"  color="primary">Login</Button>
+        <TextField name="username" value={username} onChange={(e) => setUsername(e.target.value)} id="filled-basic" label="User Name" variant="filled" />
+        <TextField name="password" value={password} onChange={(e) => setPassword(e.target.value)} id="filled-basic" label="Password" variant="filled" />
+        <Button onClick={ (e) => {
+        e.preventDefault();
+        props.login({username, password})}
+      }  color="primary">Login</Button>
         
       </form>
       
-      <form onSubmit={handleCreateAccount} className={classes.root} noValidate autoComplete="off">
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        props.createAccount(e)} 
+      }
+        className={classes.root} noValidate autoComplete="off">
       <FormLabel component="legend">Create Account</FormLabel>
-        <TextField id="filled-basic" label="User Name" variant="filled" onChange={handleChange}/>
-        <TextField id="filled-basic" label="Password" variant="filled" onChange={handleChange} />
+        <TextField name="username" value="" id="filled-basic" label="User Name" variant="filled" onChange={
+          (e) => {
+            
+            // handleChange(e);
+          }
+        }/>
+        <TextField name="password" value="" id="filled-basic" label="Password" variant="filled" onChange={
+          (e) => {
+        
+            // handleChange(e);
+          }
+        }/>
         <Button type="submit"  color="primary">Create Account</Button>
         
       </form>
 
-     
+          
     </div>
   );
 
@@ -118,3 +122,18 @@ export default function SimpleModal() {
     </div>
   );
 }
+
+const mapStateToProps = state => {  
+
+  return {
+    userInfo: state.login,        
+  };
+};
+
+const mapDispatchToProps = { createAccount, login };
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SimpleModal);
