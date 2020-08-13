e6ba00cb2c0cadb2cf888240a2b5e639d8a1176f
Garhett Morgan <32fossil@gmail.com>
2020-08-12 21:19:06 -0700

    added slider for board

--- src/components/board.js
+++ src/components/board.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import Slider from '@material-ui/core/Slider';
 
 import Player from './player.js';
 import GameHud from './gameHUD.js';
 import Upload from './upload.js'
 import Draggable from 'react-draggable';
+import Token from './token.js'
 
 
 class Board extends React.Component {
@@ -20,12 +22,43 @@ class Board extends React.Component {
                 controlledPosition: {
                     x: -400, y: 200
                 }
+            },
+            zoomWrapper: {
+                panningEnabled: false,
+                minScale: 1,
+                maxScale: 6,
+                step: 1,
+                scale: 1,
+            },
+            playerList: [
+                {
+                    name: "Melissa",
+                    color: "blue",
+                    score: 0,
+                },
+                {
+                    name: "Dave",
+                    color: "grey",
+                    score: 0,
+                },
+                {
+                    name: "Ruhai",
+                    color: "red",
+                    score: 0,
+                }
+            ],
+            slider: {
+                value: 1,
+                min: 0,
+                step: 1,
+                max: 6,
             }
         }
         this.handleAwsRes = this.handleAwsRes.bind(this);
         this.onStart = this.onStart.bind(this);
         this.onStop = this.onStop.bind(this);
         this.handleDrag = this.handleDrag.bind(this);
+        // this.handleSliderChange = this.handleSliderChange.bind(this);
     }
 
 
@@ -48,13 +81,18 @@ class Board extends React.Component {
     };
 
     handleAwsRes = (fileName) => {
-        this.setState({maps: this.state.maps.concat(fileName.locationArray[0])})
+        this.setState({ maps: this.state.maps.concat(fileName.locationArray[0]) })
 
     }
 
+    // handleSliderChange = () => {
+    //     this.setState({zoomWrapper.scale: this.state.slider.value})
+    // }
+
     render() {
-        const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
-        const {deltaPosition, controlledPosition} = this.state.playerLocation;
+        const {value, min, step, max} = this.state.slider
+        const {panningEnabled, maxScale, minScale} = this.state.zoomWrapper;
+        const { deltaPosition, controlledPosition } = this.state.playerLocation;
         return (
             <>
                 <div>
@@ -63,15 +101,34 @@ class Board extends React.Component {
                         handleAwsRes={this.handleAwsRes}
                     />
                 </div>
+                <div className="board" style={{ position: 'fixed', height: '500px', width: '500px', position: 'relative', overflow: 'auto', padding: '0' }}>
+                    <Slider
+                        slider={{
+                            value:{value},
+                            min:1,
+                            step:1,
+                            max:6,
+                        }}
+                        // onChange={this.handleSliderChange(value)}
+                    />
+                    <TransformWrapper
+                        pan={{
+                            disabled: !panningEnabled
+                        }}
+                        step={1}
+                    >
+                        <TransformComponent>
+                            <img onClick={(event) => {
+                                console.log(event.button)
+                            }} src={this.state.maps[0]} />
+                            <GameHud />
+                            
+                            <Token
+                                playerList = {this.state.playerList}
+                            />
 
-                <div className="board" style={{height: '500px', width: '500px', position: 'relative', overflow: 'auto', padding: '0'}}>
-                    <img src="{this.state.maps[0]" />
-                    <GameHud />
-                    <Draggable bounds="parent" {...dragHandlers}>
-                        {/* <Player/> */}
-                        <div className="handle" style={{height:'25px', width:'25px'}}></div>
-                    </Draggable>
-
+                        </TransformComponent>
+                    </TransformWrapper>
                 </div>
             </>
         );
---
+++ src/components/token.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+import { Avatar } from '@material-ui/core';
+// import '../scss/tokens.scss'
+class Token extends React.Component {
+    // eventLogger = (e: MouseEvent, data: Object) => {
+    //     console.log('Event: ', e);
+    //     console.log('Data: ', data);
+    // };
+    // TODO: defaultPosition property should contain a function that ties the player token to state and our turn-taking functionality || disabled property is a boolean so we can toggle it by state.
+    // TODO: When a player's turn ends, we need set the property "axis" to the string of 'none', which stops the piece from moving.
+    render() {
+      const dragHandlers = { onStart: this.onStart, onStop: this.onStop }
+        return (
+            <>
+            {this.props.playerList.map((player, idx) => {
+                    const color = this.props.playerList[idx].color;
+                    return (
+                        <Draggable bounds="parent" {...dragHandlers}>
+                            <Avatar key={idx} className={`classes.${color}`}> {this.props.playerList[idx].name.split()[0]} </Avatar>
+                        </Draggable>
+                    )
+                })
+            }
+            </>
+        );
+    }
+}
+export default Token;
